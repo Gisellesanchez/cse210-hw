66344947b82d902b4a90b4186ad36a4459642577
Gisellesanchez <162376632+Gisellesanchez@users.noreply.github.com>
2025-04-25 14:17:38 -0600

    Final

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        Console.WriteLine();
+
+        if (gradeInt >= 90)
+        {
+            Console.WriteLine("You got an A!");
+        }
+        else if (gradeInt >= 80)
+        {
+            Console.WriteLine("You got a B!");
+        }
+        else if (gradeInt >= 70)
+        {
+            Console.WriteLine("You got a C!");
+        }
+        else if (gradeInt >= 60)
+        {
+            Console.WriteLine("You got a D!");
+        }
+        else
+        {
+            Console.WriteLine("You got an F!");
+        }
     }
+
 }
