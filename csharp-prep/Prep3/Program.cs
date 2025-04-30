cbdc9ab196cbab96f72d2f1435afefa9e4f1c485
Gisellesanchez <162376632+Gisellesanchez@users.noreply.github.com>
2025-04-29 22:33:46 -0600

    Magic Number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,14 +1,59 @@
 using System;
+using System.Drawing;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-       string response = "yes";
-       while (response == "yes")
-{
-    Console.Write("Do you want to continue? ");
-    response = Console.ReadLine() ;
-}
+    //   string response;
+    //   do {
+    //     Console.Write("Do you want to continue?");
+    //     response = Console.ReadLine();
+    //    }
+    //    while ( response == "yes"); 
+
+    //    for (int i = 0; i <10; i++)
+    //    {
+    //     Console.WriteLine(i);
+    //    }
+
+    //    for (int i = 2; i <= 20; i = i + 2)
+    //    {
+    //     Console.WriteLine(i);
+    //    }
+        
+    //   Random randomGenerator = new Random();
+    //   int number = randomGenerator.Next(1, 11);
+    //     Console.WriteLine("Guess a number between 1 and 10");
+    //     int guess = int.Parse(Console.ReadLine());
+    // 
+    
+    //     if (guess == number
+    
+
+        Console.WriteLine("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int magicNumberInt = int.Parse(magicNumber);
+        Console.WriteLine("What is your guess? ");
+        string guessString = Console.ReadLine();
+        int guess = int.Parse(guessString);
+
+        if (guess < magicNumberInt)
+        {
+            Console.WriteLine("Too low!");
+        }
+        else if (guess > magicNumberInt)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+   
+
+
     }
 }
+    
