10c629106346e4244a44f17561705d28a7151f46
Gisellesanchez <162376632+Gisellesanchez@users.noreply.github.com>
2025-05-03 19:15:41 -0600

    Update Program.cs

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,6 +6,18 @@ class Program
     {
         Console.WriteLine("Hello Sandbox World!");
 
+
+
+List<int> myNumbers + new List<int>();
+    myNumbers.Add(10);
+    myNumbers.Add(99);
+    myNumbers.Add(-123)
+
+
+foreach (int i in myNumbers);
+{
+    Console.WriteLine(i);
+}
         //This is a fun comment 
 
         //second comment
