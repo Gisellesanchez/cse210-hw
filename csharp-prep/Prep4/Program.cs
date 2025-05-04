83e3d5cbc2b0b3e98f15ee7e88dfbaf0a3ea3c54
Gisellesanchez <162376632+Gisellesanchez@users.noreply.github.com>
2025-05-03 20:45:40 -0600

    Enter numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
+using System.Collections.Generic;
 using System;
-
+using System.Transactions;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        //Practice Prep 4
+        // List<int> numbers = new List<int>();
+        // List<string> words = new List<string>();
+        
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("mouse");
+
+        // Console.WriteLine(words.Count);
+
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+
+        // for (int i = 0; i < words.Count; i++)
+        // {
+        //     Console.WriteLine(words[i]);
+
+        // }
+    
+    List<int> numbers = new List<int>();
+    int input;
+
+    Console.WriteLine("Enter a list of numbers, type 0 when you are finished.");
+
+  
+
     }
 }
