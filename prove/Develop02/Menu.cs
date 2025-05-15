78731fae5f236c38025eb9328e3697b08d3d6067
Gisellesanchez <162376632+Gisellesanchez@users.noreply.github.com>
2025-05-15 10:24:09 -0600

    MenuDisplay

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -2,9 +2,22 @@ using System;
 
 class Menu
 {
-     public void DisplayTitle()
+    public void DisplayTitle()
 
 
     {
         Console.WriteLine("Welcome to the Journal Program!");
-    } }
+        Console.WriteLine(" Write, Display, Save and Load your Journal Entries");
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine("This program allows you to create and manage your journal entries.");
+    }
+
+    public void DisplayMenu()
+    {
+        Console.WriteLine("Please select an option:");
+        Console.WriteLine();
+        Console.WriteLine("1. Create a new journal entry");
+        Console.WriteLine("2. View existing journal entries");
+        Console.WriteLine("3. Exit");
+    }
+}
